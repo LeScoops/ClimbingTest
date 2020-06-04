0bc049377c2c5f75c58d8ff30ff3ec23cf2449ba
Ben <34342294+LeScoops@users.noreply.github.com>
2020-06-04 13:09:51 -0400

    06/04/20

---
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame() { SceneManager.LoadScene("TestScene01", LoadSceneMode.Single); }
+    public void Instructions() { SceneManager.LoadScene("Instructions", LoadSceneMode.Single); }
+    public void MainMenu() { SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); }
+    public void QuitGame() { Application.Quit(); }
+}
