844ba2635be4eb2624c7b33a8eabec5c7a412730
Ben <34342294+LeScoops@users.noreply.github.com>
2020-06-06 08:39:55 -0400

    06/06/20

--- Assets/Scripts/Player/Gliding.cs
+++ Assets/Scripts/Player/Gliding.cs
@@ -7,7 +7,7 @@ public class Gliding : MonoBehaviour
     [SerializeField] float glidingStaminaRequirement = -5.0f;
     [SerializeField] float glidingGravity = -4.0f;
     [SerializeField] float baseGlidingSpeed = 2.0f;
-    [SerializeField] float maxGlidingSpeed = 20.0f;
+    [SerializeField] float maxGlidingSpeed = 10.0f;
     float currentGlidingSpeed = 2.0f;
 
     public void GlidingMovement(CharacterController controller, float delta)
